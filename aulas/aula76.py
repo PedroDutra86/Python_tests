6fea1d7cee6ad24d36055bd576f6fc8d9f352eae
Pedro Dutra <154842345+PedroDutra86@users.noreply.github.com>
2024-08-27 14:23:44 +0000

    adicao de mais aulas

---
+++ aulas/aula76.py
@@ -0,0 +1,44 @@
+
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+p1 = {
+    'nome': 'Pedro',
+    'sobrenome': 'Dutra',
+}
+
+# p1.update({
+#     'nome1': 'AAAA',
+#     'sobrenome1': 'BBBB',
+#     'idade': 123,
+# })
+# p1.update(idade=123, nome='Qualquer outro')
+variavel = (1,)
+print(variavel)
+p1.update(
+    (
+        ('Chave', 'Valor'),
+        ('Chave1', 'Valor1'),
+        ('Chave2', 'Valor1'),
+    )
+)
+print(p1)
+# print(p1.get('nome', 'Valor padrão'))
+
+# nome = p1.pop('nome')
+# print(nome)
+# print(p1)
+
+# ultima_chave = p1.popitem()
+# print(ultima_chave)
+# ultima_chave = p1.popitem()
+# print(ultima_chave)
+# print(p1)
